f2188d31e7feea7ddb9b7ae386f8ea9d099b0055
Ali Torabi <alitorabi2020@gmail.com>
2019-12-15 23:03:00 +0330

    Adding Guarantee

---
+++ SystemDesign/Controllers/GuaranteeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using SystemDesign.Models;
+
+namespace SystemDesign.Controllers
+{
+    public class GuaranteeController : Controller
+    {
+        public static List<Guarantee> Guarantees = new List<Guarantee>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post(Guarantee guarantee)
+        {
+            if (ModelState.IsValid)
+            {
+                Guarantees.Add(guarantee);
+
+                return new JsonResult("The guarantee added successfully");
+            }
+            else
+            {
+                return new JsonResult("The guarantee had errors");
+            }
+        }
+
+        public IActionResult Get()
+        {
+            return new JsonResult(Guarantees);
+        }
+
+        public IActionResult GetById(int id)
+        {
+            return new JsonResult(Guarantees.Where(p => p.Id == id).FirstOrDefault());
+        }
+    }
+}
---
+++ SystemDesign/Models/Guarantee.cs
@@ -0,0 +1,10 @@
+﻿namespace SystemDesign.Models
+{
+    public class Guarantee
+    {
+        public int Id { get; set; }
+        public long Price { get; set; }
+        public string SourceName { get; set; }
+        public string DestinationName { get; set; }
+    }
+}
